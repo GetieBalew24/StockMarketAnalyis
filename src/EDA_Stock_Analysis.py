46fef581406ec2d25cabe130a6d208775ad4983d
GetieBalew24 <getiebalew00@gmail.com>
2024-12-12 23:25:36 +0300

    Analyze the publication dates

--- src/EDA_Stock_Analysis.py
+++ src/EDA_Stock_Analysis.py
@@ -51,3 +51,46 @@ class StockMarketAnalysis:
         publisherCounts.columns = ['Publisher', 'Article Count']
         print("Article Counts per Publisher:")
         return publisherCounts
+    
+    def plot_article_per_publisher(self):
+        publisherCounts = self.article_per_publisher().nlargest(5, 'Article Count')
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x='Publisher', y='Article Count', data=publisherCounts)
+        plt.xticks(rotation=90)
+        plt.xlabel('Publisher')
+        plt.ylabel('Number of Articles')
+        plt.title('Number of Articles for each Publisher')
+        plt.show()
+    
+    def analyze_publication_dates(self):
+        """Analyze publication dates to identify trends over time."""
+        self.data['date'] = pd.to_datetime(self.data['date'], errors='coerce',utc=True)
+        self.data['year'] = self.data['date'].dt.year
+        self.data['month'] = self.data['date'].dt.month
+        self.data['day_of_week'] = self.data['date'].dt.day_name()
+
+        # Articles over time by year and month
+        articlesOverTime = self.data.groupby(['year', 'month']).size().reset_index(name='article_count')
+
+        # Articles by day of the week
+        articlesByDay = self.data['day_of_week'].value_counts()
+
+        return articlesOverTime, articlesByDay
+    
+    def plot_article_trends(self, articles_over_time, articles_by_day):
+        """Plot trends of article counts over time and by day of the week."""
+        # Plotting article counts over time
+        plt.figure(figsize=(12, 6))
+        sns.lineplot(data=articles_over_time, x='month', y='article_count', hue='year', marker='o')
+        plt.title('Article Count Time')
+        plt.xlabel('Month')
+        plt.ylabel('Number of Articles')
+        plt.show()
+
+        # Plotting article counts by day of the week
+        plt.figure(figsize=(10, 5))
+        sns.barplot(x=articles_by_day.index, y=articles_by_day.values)
+        plt.title('Article Count by Day of the Week')
+        plt.xlabel('Day of the Week')
+        plt.ylabel('Number of Articles')
+        plt.show()
